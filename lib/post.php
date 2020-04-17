ebd9d1ff72afcafde9f60ca0622f970a2e1e8528
Mike van Essen <mve10@msn.com>
2020-04-17 15:00:15 +0200

    feat: added all data made public from yoast except slug

--- lib/post.php
+++ lib/post.php
@@ -19,17 +19,19 @@ if( !class_exists('Post') ) :
             $permalink = get_permalink($post_id);
             $modules = $this->checkWhenToSwapWithDefault($postMeta['modules']);
             $taxonomies = wp_get_post_terms($post_id, get_taxonomies('','names'));
-
+            $metaData = $this->getMetaData($post_id);
+            
             return array(
                 '_id' => $blogId . '-post-' . $post->ID,
                 'blogId' => $blogId,
                 'type' => 'post',
                 'taxonomies' => $taxonomies,
+                'meta' => $metaData,
                 'data' => array(
                     'post' => $post,
                     'permalink' => $permalink,
                     'modules' => $modules
-                )
+                ),
             );
         }
 
@@ -56,6 +58,24 @@ if( !class_exists('Post') ) :
             }
     
             return $result;
+        }
+
+        function getMetaData($post_id) {
+            return array(
+                'seoTitle' => get_post_meta($post_id, '_yoast_wpseo_title', 'SEO Title'),
+                'seoMetaDescription' => get_post_meta($post_id, '_yoast_wpseo_metadesc', 'SEO Meta Description'),
+                'focusKeyword' => get_post_meta($post_id, '_yoast_wpseo_focuskw', 'SEO Focus keyword'),
+                'siteMap' => get_post_meta($post_id, '_yoast_wpseo_sitemap-include', ''),
+                'siteMapPrio' => get_post_meta($post_id, '_yoast_wpseo_sitemap-prio  ', 'SEO Focus keyword'),
+                'canonicalUrl' => get_post_meta($post_id, '_yoast_wpseo_canonical ', 'Canonical Url'),
+                'redirectUrl' => get_post_meta($post_id, '_yoast_wpseo_redirect', 'Redirect url'),
+                'facebookTitle' => get_post_meta($post_id, '_yoast_wpseo_opengraph-title ', 'Facebook Title'),
+                'facebookDescription' => get_post_meta($post_id, '_yoast_wpseo_opengraph ', 'Facebook Description'),
+                'facebookImage' => get_post_meta($post_id, '_yoast_wpseo_opengraph-image ', 'Image Url'),
+                'metaRobotIndex' => get_post_meta($post_id, '_yoast_wpseo_meta-robots-noindex', ''),
+                'metaRobotFollow' => get_post_meta($post_id, '_yoast_wpseo_meta-robots-nofollow', ''),
+                'metaRobotAdvanced' => get_post_meta($post_id, '_yoast_wpseo_meta-robots-adv', ''),
+            );
         }
     }
 endif;
